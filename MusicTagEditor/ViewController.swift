f565bc121941c2aabf34383df954bad42df9fe3f
Kavin <kavin.inbox@outlook.com>
2020-01-15 22:08:48 +0530

    Initial Commit

---
+++ MusicTagEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MusicTagEditor
+//
+//  Created by Kavin K on 15/01/20.
+//  Copyright © 2020 Kavin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
